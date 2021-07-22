04136e23d28f3bff3ab3096c599dab1f2a71799d
Mohammed Rayan sailani <rayanbackup1@gmail.com>
2021-07-22 20:02:57 +0530

    changed urls

--- api/urls.py
+++ api/urls.py
@@ -4,8 +4,8 @@ from . import views
 urlpatterns = [
     path('', views.apiOverView, name="overview"),
     path('tasks', views.taskList, name="taskList"),
-    path('task/<str:pk>/', views.detailView, name="detailView"),
-    path('create/', views.createView, name = 'createView'),
-    path('update/<str:pk>/', views.updateView, name = 'updateView'),
-    path('delete/<str:pk>/', views.deleteView, name="deleteView"),
+    path('task/<str:pk>', views.detailView, name="detailView"),
+    path('create', views.createView, name = 'createView'),
+    path('update/<str:pk>', views.updateView, name = 'updateView'),
+    path('delete/<str:pk>', views.deleteView, name="deleteView"),
 ]
